d74ff3453322dee47d85aa6880d0336bc0227448
Felix Hammerl <fhammerl@thoughtworks.com>
2018-03-05 22:27:25 +0530

    Make github API token less intrusive

--- bash.sh
+++ bash.sh
@@ -8,13 +8,6 @@ DIR=$(cd "$(dirname "$0")"; pwd)
 echo $DIR
 cp $DIR/.bash_profile ~
 
-while true; do
-  read -p  "Please enter the HOMEBREW_GITHUB_API_TOKEN before proceeding!" HOMEBREW_GITHUB_API_TOKEN
-  if [ -z "$HOMEBREW_GITHUB_API_TOKEN" ]; then
-    echo "Github API Token is missing."
-    exit 1
-  fi
-done
-
+read -p  "Please enter the HOMEBREW_GITHUB_API_TOKEN before proceeding!" HOMEBREW_GITHUB_API_TOKEN
 echo "export HOMEBREW_GITHUB_API_TOKEN=\"$HOMEBREW_GITHUB_API_TOKEN\"" >> ~/.bash_profile
 
