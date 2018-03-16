d2a28bec34323bd617a2ef0dd9e4517223a9f8bb
Felix Hammerl <fhammerl@thoughtworks.com>
2018-03-16 15:14:51 +0530

    install scm-breeze

--- bash.sh
+++ bash.sh
@@ -8,6 +8,9 @@ DIR=$(cd "$(dirname "$0")"; pwd)
 echo $DIR
 cp $DIR/.bash_profile ~
 
+git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
+~/.scm_breeze/install.sh
+
 read -p  "Please enter the HOMEBREW_GITHUB_API_TOKEN before proceeding!" HOMEBREW_GITHUB_API_TOKEN
 echo "export HOMEBREW_GITHUB_API_TOKEN=\"$HOMEBREW_GITHUB_API_TOKEN\"" >> ~/.bash_profile
 
