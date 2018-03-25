e48982d87c2233dacdbb730b68f3a51af16ccd8c
Felix Hammerl <felix.hammerl@gmail.com>
2018-03-25 20:40:08 +0200

    Remove comment

--- setup.sh
+++ setup.sh
@@ -11,8 +11,3 @@ set -eu
 
 echo -e "Please install the following software manually: \n\nShush (App Store)\nTicToc (AppStore)"
 
-echo "Please install the following fonts on your system:"
-echo "Open Sans"
-echo "Adobe Source Code Pro (https://github.com/adobe-fonts/source-code-pro/releases/latest)"
-echo "Fira Code (https://github.com/tonsky/FiraCode/releases/latest)"
-
