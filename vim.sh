73c64e507e2ad973ce43153141b430328c205025
Felix Hammerl <fhammerl@thoughtworks.com>
2018-03-28 13:19:52 +0200

    make vim swap folder only after cloning repos

--- vim.sh
+++ vim.sh
@@ -6,12 +6,12 @@ echo "Cleaning .vim folder..."
 
 rm -rfv ~/.vim
 rm -f ~/.vimrc
-mkdir -p ~/.vim
-mkdir -p ~/.vim/swap
 
 echo "Setting up .vim folder..."
 git clone git@github.com:felixhammerl/vim.git ~/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
 ~/.vim/update.sh
+
+mkdir -p ~/.vim/swap
 
 echo "vim is ready to use!"
