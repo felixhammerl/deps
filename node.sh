e363f5dabebdf883801758d7e5d2cef2fbc67a0a
Felix Hammerl <fhammerl@thoughtworks.com>
2018-04-02 18:49:41 +0200

    do not fail stupidly

--- node.sh
+++ node.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -eu
-
 echo "Installing node modules..."
 
 npm install -g n
--- ruby.sh
+++ ruby.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -eu
-
 echo "Installinging ruby gems..."
 
 gem install jekyll
