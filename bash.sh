0eb84e6eff772ecf37bc747873bf8fe1ad450fcc
Felix Hammerl <felix.hammerl@gmail.com>
2018-06-20 09:21:48 +0200

    wire up tmux conf

--- bash.sh
+++ bash.sh
@@ -7,6 +7,7 @@ echo "Configuring bash..."
 DIR=$(cd "$(dirname "$0")"; pwd)
 echo "working relative to $DIR"
 cp $DIR/.bash_profile ~
+cp $DIR/.tmux.conf ~
 
 read -p  "Please enter the HOMEBREW_GITHUB_API_TOKEN before proceeding!" HOMEBREW_GITHUB_API_TOKEN
 echo "export HOMEBREW_GITHUB_API_TOKEN=\"$HOMEBREW_GITHUB_API_TOKEN\"" >> ~/.bash_profile
