466c7a05647790b82c0c22db8ef73c7cb145b8af
Felix Hammerl <fhammerl@thoughtworks.com>
2018-10-24 00:51:55 +0300

    Setup pinentry for gpg

---
+++ gpg.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -eu
+
+mkdir -p ~/.gnupg
+echo "pinentry-program /usr/local/bin/pinentry-mac" > ~/.gnupg/gpg-agent.conf
+echo "use-agent" > ~/.gnupg/gpg.conf
+gpgconf --kill gpg-agent
+
