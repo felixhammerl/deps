005691958f066df72916922528fcf09ec1ddc23c
Felix Hammerl <fhammerl@thoughtworks.com>
2018-11-30 11:24:12 +0100

    add bitbar to deps

--- macos.sh
+++ macos.sh
@@ -9,16 +9,19 @@ if [ $# != 0 ]; then
   xcode-select --install
 fi
 
-ROOTLESS_STATUS=`/usr/bin/csrutil status | awk '/status/ {print $5}' | sed 's/\.$//'`
+ROOTLESS_STATUS=$(/usr/bin/csrutil status | awk '/status/ {print $5}' | sed 's/\.$//')
 if [[ $ROOTLESS_STATUS == "enabled" ]]; then
   echo "csrutil (\"rootless\") is enabled. please disable in boot screen and run again!"
   exit 1
 fi
 
-FILEVAULT_STATUS=`fdesetup status`
+FILEVAULT_STATUS=$(fdesetup status)
 if [[ $FILEVAULT_STATUS != "FileVault is On." ]]; then
   echo "FileVault is not turned on. Please encrypt your hard disk!"
 fi
+
+echo "Creating projects folder"
+mkdir -p ~/projects
 
 # echo "Configuring non-crashing safe sleep/hibernate settings"
 # echo "Restoring defaults before applying new config"
--- setup.sh
+++ setup.sh
@@ -7,18 +7,19 @@ echo "Welcome to the grand environment setup extravaganza."
 DIR=$(cd "$(dirname "$0")"; pwd)
 echo "working relative to $DIR"
 
-$DIR/macos.sh
-$DIR/bash.sh
-$DIR/brew.sh
-$DIR/node.sh
-$DIR/rust.sh
-$DIR/ruby.sh
-$DIR/python.sh
-$DIR/neovim.sh
-$DIR/vscode.sh
-$DIR/git.sh
+"$DIR"/macos.sh
+"$DIR"/bash.sh
+"$DIR"/brew.sh
+"$DIR"/node.sh
+"$DIR"/rust.sh
+"$DIR"/ruby.sh
+"$DIR"/python.sh
+"$DIR"/neovim.sh
+"$DIR"/vscode.sh
+"$DIR"/git.sh
+"$DIR"/bitbar.sh
 
-echo -e "Please install the following software manually:\n\nCinch (AppStore)\nShush (App Store)\nTicToc (AppStore)"
+echo -e "Please install the following software manually:\\n\\nCinch (AppStore)\\nShush (App Store)\\nTicToc (AppStore)"
 
-echo -e "Please enable Fira Code and font ligatures and install the following IDEA plugins:\n\nIdeaVim\nRust\nScala\nToml\nMaterial Theme UI
+echo -e "Please enable Fira Code and font ligatures and install the following IDEA plugins:\\n\\nIdeaVim\\nRust\\nScala\\nToml\\nMaterial Theme UI
 "
