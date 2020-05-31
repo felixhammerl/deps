86062eb8ba4797d8a0f4295355d33213a69a7229
Felix Hammerl <felix.hammerl@gmail.com>
2020-05-31 22:03:16 +0200

    Ignore Github API token for homebrew

--- shell.sh
+++ shell.sh
@@ -7,10 +7,5 @@ echo "Configuring bash..."
 DIR=$(cd "$(dirname "$0")"; pwd)
 echo "working relative to $DIR"
 
-read -p  "Please enter the HOMEBREW_GITHUB_API_TOKEN before proceeding!" HOMEBREW_GITHUB_API_TOKEN
-echo "export HOMEBREW_GITHUB_API_TOKEN=\"$HOMEBREW_GITHUB_API_TOKEN\"" >> ~/.zshrc
-cat $DIR/.zshrc >> ~/.zshrc
-
 cp $DIR/.tmux.conf ~
-
 
