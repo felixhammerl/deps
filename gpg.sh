84176be3043641ce72288721d107e91d9506c510
Felix Hammerl <felix.hammerl@gmail.com>
2020-05-31 22:03:42 +0200

    Fuck GPG

--- gpg.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-set -eu
-
-mkdir -p ~/.gnupg
-echo "pinentry-program /usr/local/bin/pinentry-mac" > ~/.gnupg/gpg-agent.conf
-echo "use-agent" > ~/.gnupg/gpg.conf
-gpgconf --kill gpg-agent
-
