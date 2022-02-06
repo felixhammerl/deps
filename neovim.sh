59ad290ee77a8be8af498492ecc5e2b752e6e449
Felix Hammerl <felix.hammerl@gmail.com>
2022-02-06 21:09:31 +0100

    Add python coc snippets to be pulled automatically

--- neovim.sh
+++ neovim.sh
@@ -10,10 +10,12 @@ mkdir -p ~/.config/nvim
 mkdir -p ~/.nvim/undo
 mkdir -p ~/.nvim/swp
 mkdir -p ~/.nvim/backup
+mkdir -p ~/.config/coc/ultisnips/
 
 echo "Setting up neovim..."
 curl https://raw.githubusercontent.com/felixhammerl/vim/master/init.vim > ~/.config/nvim/init.vim
 curl https://raw.githubusercontent.com/felixhammerl/vim/master/coc-settings.json > ~/.config/nvim/coc-settings.json
+curl https://raw.githubusercontent.com/felixhammerl/vim/master/python.snippets > ~/.config/coc/ultisnips/python.snippets
 
 echo "Replacing vim with neovim"
 ln -s /opt/homebrew/bin/nvim /usr/local/bin/vi
