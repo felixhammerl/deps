fbae868ef49ff7d1c1e9d9d0a9ebe653406947db
Felix Hammerl <felix.hammerl@gmail.com>
2022-02-16 18:53:22 +0100

    Update vim python install

--- python.sh
+++ python.sh
@@ -3,10 +3,16 @@
 echo "Installing python dependencies..."
 
 pip3 install --upgrade pip
-pip3 install pynvim virtualenv
+pip3 install virtualenv
 
 pyenv install 3.8.0
 pyenv install 3.10.0
+
+mkdir -p ~/.virtualenv
+cd ~/.virtualenv
+python3 -m venv ./neovim
+source neovim/bin/activate
+pip install --upgrade pynvim
 
 echo "Done!"
 
