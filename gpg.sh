5543cc67d938891a7396a7a14bd7bb99e6f2356e
Felix Hammerl <felix.hammerl@gmail.com>
2022-11-02 11:55:35 +0100

    haaaaaands

--- gpg.sh
+++ gpg.sh
@@ -5,8 +5,8 @@ set -eu
 echo "Configuring GPG..."
 
 mkdir -p ~/.gnupg
-cp $DIR/gpg.conf ~/.gnupg
-cp $DIR/gpg-agent.conf ~/.gnupg
+cp gpg.conf ~/.gnupg
+cp gpg-agent.conf ~/.gnupg
 export GNUPGHOME=~/.gnupg
 
 gpg --keyserver pgp.mit.edu --recv-keys 7859E6520888D02E
