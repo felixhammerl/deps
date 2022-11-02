6b36672343a0def6685d160a675e41d7447674f8
Felix Hammerl <felix.hammerl@gmail.com>
2022-11-02 10:18:37 +0100

    haaaaaands

--- neovim.sh
+++ neovim.sh
@@ -29,4 +29,7 @@ source neovim/bin/activate
 pip install --upgrade pynvim
 deactivate
 
+echo "Install vim plug"
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 echo "done!"
