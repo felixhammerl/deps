b08dcb6aeb59ac6958f53317bbc36beaf8c7fb28
Felix Hammerl <felix.hammerl@gmail.com>
2023-07-28 23:14:26 +0200

    Add gpg dependency on ubuntu

--- linux/gpg.sh
+++ linux/gpg.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+echo "Installing dependencies ..."
+
+sudo apt install scdaemon
+
 echo "Configuring GPG..."
 
 mkdir -p ~/.gnupg
@@ -10,6 +14,8 @@ cp gpg-agent.conf ~/.gnupg
 export GNUPGHOME=~/.gnupg
 
 gpg --keyserver keyserver.ubuntu.com --recv-keys 7859E6520888D02E
+
+gpg-connect-agent "scd serialno" "learn --force" /bye
 
 cat >> ~/.zshrc <<HERE
 
