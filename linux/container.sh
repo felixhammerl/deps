3183e38166f8866639d746c8ffa5a419e291685d
Felix Hammerl <felix.hammerl@gmail.com>
2024-04-03 02:14:36 +0200

    Add CNI plugins

--- linux/container.sh
+++ linux/container.sh
@@ -18,15 +18,33 @@ sudo systemctl status docker
 
 echo "Installing nerdctl..."
 
-echo "Go to https://github.com/containerd/nerdctl/releases and get the latest version"
+download_url="https://github.com/containerd/nerdctl/releases"
+echo "Go to $download_url and get the latest version"
 
-read -r "version?Latest stable Nerdctl version: "
+read -r "version?Latest stable version: "
 filename="nerdctl-$version-linux-amd64.tar.gz"
-wget "https://github.com/containerd/nerdctl/releases/download/v$version/$filename"
+wget "$download_url/download/v$version/$filename"
 tar Cxzvvf $HOME/bin "$filename"
+
+rm $filename
 
 sudo chown root $HOME/bin/nerdctl
 sudo chmod +s $HOME/bin/nerdctl
+
+echo "Installing CNI plugins ..."
+
+sudo mkdir -p /opt/cni/bin/
+
+download_url="https://github.com/containernetworking/plugins/releases"
+echo "Go to $download_url and get the latest version"
+
+read -r "version?Latest stable Nerdctl version: "
+filename="cni-plugins-linux-amd64-v$version.tgz"
+wget "$download_url/download/v$version/$filename"
+
+sudo tar Cxzvf /opt/cni/bin "$filename"
+
+rm $filename
 
 cat >> ~/.zshrc <<HERE
 
@@ -34,7 +52,7 @@ cat >> ~/.zshrc <<HERE
 # Container settings
 # 
 
-export PATH=\$HOME/bin:\$PATH
+export PATH=\$HOME/bin:/opt/cni/bin:\$PATH
 
 HERE
 
