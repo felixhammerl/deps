afc9dde02e37c1693b4f10ae2c1f257f2fae8cb5
Felix Hammerl <felix.hammerl@gmail.com>
2024-08-13 11:20:36 +0200

    Move VSCode installation to apt.sh

--- linux/vscode.sh
+++ linux/vscode.sh
@@ -2,16 +2,7 @@
 
 set -eu
 
-echo "Installing vscode ..."
-
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-
-sudo apt install -y apt-transport-https
-sudo apt update
-sudo apt install -y code
+echo "Installing vscode plugins..."
 
 mkdir -p $HOME/.config/Code/User/snippets
 cp vscode-settings.json $HOME/.config/Code/User/settings.json
