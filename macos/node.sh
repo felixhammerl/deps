e3c22e5c46090eb12a96e2e7fd8a3229e6782434
Felix Hammerl <felix.hammerl@gmail.com>
2025-02-05 15:36:47 +0100

    Update node shell settings

--- macos/node.sh
+++ macos/node.sh
@@ -2,9 +2,20 @@
 
 echo "Installing node latest and LTS..."
 
+cat >> ~/.zshrc <<HERE
+
+#
+# Node settings
+# 
+
+export NODE_OPTIONS="--no-experimental-require-module"
+
+HERE
+
 nvm install node
 nvm install --lts
 nvm use node
+
 
 echo "Installing node modules..."
 
