48d14db69b66d99735b3b00e3d3c95ccf9f0ef42
Felix Hammerl <felix.hammerl@gmail.com>
2025-03-06 15:29:42 +0100

    Do not install poe as poetry plugin

--- macos/poetry.sh
+++ macos/poetry.sh
@@ -7,5 +7,4 @@ curl -sSL https://install.python-poetry.org | python3 -
 poetry self add poetry-plugin-export
 poetry self add poetry-plugin-shell
 poetry self add poetry-plugin-up
-poetry self add 'poethepoet[poetry_plugin]'
 poetry self update
