94488a229bc5be24e1196b8ae4c6e3a4c30538cb
Felix Hammerl <felix.hammerl@gmail.com>
2025-04-09 09:45:36 +0200

    Add switch whether command line tools have been installed

--- macos/macos.sh
+++ macos/macos.sh
@@ -4,10 +4,17 @@ set -eu
 
 echo "Preparing macOS..."
 
-echo "Installing Xcode command line tools..."
-xcode-select --install
-echo "Press enter to continue when Xcode is done installing..."
-read yn
+if xcode-select -p &>/dev/null; then
+    echo "Xcode Command Line Tools already installed"
+else
+    echo "Installing Xcode Command Line Tools..."
+    xcode-select --install
+    while ! xcode-select -p &>/dev/null; do
+        echo "Waiting for Xcode Command Line Tools finish installing..."
+        sleep 5
+    done
+fi
+
 
 echo "Do not write .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
